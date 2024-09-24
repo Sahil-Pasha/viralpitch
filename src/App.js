a8896cc7e140e34524e5ba6aad64de1596bb7011
farhan <turkfarhan1998@gmail.com>
2024-09-24 21:39:01 +0530

    "Viral-Pitch"

--- src/App.js
+++ src/App.js
@@ -1,25 +1,54 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.css'
+import paidCollabs from '../src/assets/images/paid-brand.png'
+import celebrityCollabs from '../src/assets/images/celeb-collab.png'
+import referFriend from '../src/assets/images/refer-friend.png'
+import launchBrand from '../src/assets/images/launch-brand.png'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+      <div className="main-container">
+        <p className="section-heading">
+          but that’s not all. there’s so much{' '}
+          <span className="more-text">more</span> you get.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <div className="image-container">
+          <div>
+            <img src={paidCollabs} className="zoom-in" alt="Paid Collabs" />
+            <p className="section-text">
+              <span className="more-text">more</span> paid brand collabs
+            </p>
+          </div>
+          <div>
+            <img
+              src={celebrityCollabs}
+              className="zoom-in"
+              alt="Celebrity Collabs"
+            />
+            <p className="section-text">
+              <span className="more-text">more</span> celebrity collabs
+            </p>
+          </div>
+          <div>
+            <img src={referFriend} className="zoom-in" alt="Refer Friends" />
+            <p className="section-text">
+              refer friends for <span className="more-text">more</span> returns
+            </p>
+          </div>
+          <div>
+            <img
+              src={launchBrand}
+              className="zoom-in"
+              alt="Launch Your Brand"
+            />
+            <p className="section-text">
+              launch your own brand & <span className="more-text">more</span>
+            </p>
+          </div>
+        </div>
+      </div>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
